b3237da1ba4ae3e311b7c4e6b2aa9978491c0b16
Mike Dillon <mike.dillon@synctree.com>
2014-10-10 19:23:24 +0000

    Copy envvar code from wordpress Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+set -e
+
+if [ -z "$MEDIAWIKI_DB_HOST" -a -z "$MYSQL_PORT_3306_TCP" ]; then
+	echo >&2 'error: missing MYSQL_PORT_3306_TCP environment variable'
+	echo >&2 '  Did you forget to --link some_mysql_container:mysql ?'
+	exit 1
+fi
+
+# if we're linked to MySQL, and we're using the root user, and our linked
+# container has a default "root" password set up and passed through... :)
+: ${MEDIAWIKI_DB_USER:=root}
+if [ "$MEDIAWIKI_DB_USER" = 'root' ]; then
+	: ${MEDIAWIKI_DB_PASSWORD:=$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
+fi
+: ${MEDIAWIKI_DB_NAME:=mediawiki}
+
+if [ -z "$MEDIAWIKI_DB_PASSWORD" ]; then
+	echo >&2 'error: missing required MEDIAWIKI_DB_PASSWORD environment variable'
+	echo >&2 '  Did you forget to -e MEDIAWIKI_DB_PASSWORD=... ?'
+	echo >&2
+	echo >&2 '  (Also of interest might be MEDIAWIKI_DB_USER and MEDIAWIKI_DB_NAME.)'
+	exit 1
+fi
+
 if ! [ -e index.php -a -e includes/DefaultSettings.php ]; then
 	echo >&2 "MediaWiki not found in $(pwd) - copying now..."
 
@@ -20,5 +44,7 @@ if ! [ -e index.php -a -e includes/DefaultSettings.php ]; then
 		EOF
 	fi
 fi
+
+chown -R "$APACHE_RUN_USER:$APACHE_RUN_GROUP" .
 
 exec "$@"
